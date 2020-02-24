01876e6cc2db916e37965f99094e81fbce9cff13
Laukik Rupne <laukikrupne11@gmail.com>
2020-02-25 03:08:35 +0530

    final

---
+++ plant_app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from keras.preprocessing import image
+import numpy as np
+from .deeplearning import graph, model, output_list
+import base64
+import cv2
+
+
+
+
+def index(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        b64_img = base64.b64encode(myfile.file.read()).decode('ascii')
+        img = image.load_img(myfile, target_size=(128, 128))
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = img/255
+
+        with graph.as_default():
+            prediction = model.predict(img)
+
+        prediction_flatten = prediction.flatten()
+        max_val_index = np.argmax(prediction_flatten)
+        result = output_list[max_val_index]
+
+        return render(request, "plant_app/index.html", {
+            'result': result, 'file_url': b64_img })
+
+    return render(request, "plant_app/index.html")
